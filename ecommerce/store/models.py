0a9c9b8f9e8b064ca95a62f6e210ab12e3d59a98
[ishkining] <ishkining@gmail.com>
2023-02-09 16:18:45 +0500

    1th hour of touching the course. Briefly we have done baseline installation and setup; django installation and setup; building our models

---
+++ ecommerce/store/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=250, db_index=True)
+
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+
+        verbose_name_plural = 'categories'
+
+    def __str__(self) -> str:
+
+        return self.name
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=250)
+
+    brand = models.CharField(max_length=250, default='un_branded')
+
+    description = models.TextField(blank=True)
+
+    slug = models.SlugField(max_length=250)
+
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+
+        verbose_name_plural = 'products'
+
+    def __str__(self) -> str:
+
+        return self.title
